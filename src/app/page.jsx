2ebe17ec2c0741913cfb9213b002c42eae02eb47
chinmoybiswas93 <chinmoy709@gmail.com>
2025-04-12 09:08:52 +0600

    added layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,93 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello From Main Branch</div>;
+  return (
+    <div>
+      <Link href="/blog" className={styles.link}>
+        <h1 className={styles.title}>Blog App</h1>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet rerum
+          recusandae distinctio id possimus cupiditate laborum unde sint
+          laboriosam. Quisquam minima culpa ex praesentium itaque quo beatae
+          quod tempora? Fugiat deleniti ratione iure in, ducimus deserunt
+          expedita laudantium sunt sint iste eum ad nemo autem consequuntur
+          recusandae adipisci? Vitae, fugit?
+        </h2>
+        <h3>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis
+          expedita eos incidunt? Quae, eius. Eaque officiis excepturi cum non
+          eligendi vel tenetur ipsum obcaecati, tempore qui delectus porro
+          nostrum sapiente nobis exercitationem eos est fuga doloremque maiores
+          quas aliquam laudantium?
+        </h3>
+        <p className={styles.description}>The best blog app!</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam natus
+          voluptatem totam ad, nam, voluptates labore at expedita rerum rem
+          temporibus illum sit ut quisquam. Animi voluptas nisi veniam odio sit.
+          Enim aliquid odio eos ratione odit optio omnis ipsam fugit blanditiis
+          facere culpa, labore modi quas, nobis maiores voluptatibus libero
+          dolor, obcaecati magni iure quaerat quae autem? Rem voluptatibus
+          laborum quis eum error dolores! Est inventore voluptatibus
+          perspiciatis dolorum voluptas ab eum libero quas consequatur iure modi
+          iste accusamus veniam dolor architecto veritatis maxime eos minima,
+          assumenda nemo dignissimos ipsa suscipit neque delectus! Iste earum
+          porro voluptate illo nostrum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam natus
+          voluptatem totam ad, nam, voluptates labore at expedita rerum rem
+          temporibus illum sit ut quisquam. Animi voluptas nisi veniam odio sit.
+          Enim aliquid odio eos ratione odit optio omnis ipsam fugit blanditiis
+          facere culpa, labore modi quas, nobis maiores voluptatibus libero
+          dolor, obcaecati magni iure quaerat quae autem? Rem voluptatibus
+          laborum quis eum error dolores! Est inventore voluptatibus
+          perspiciatis dolorum voluptas ab eum libero quas consequatur iure modi
+          iste accusamus veniam dolor architecto veritatis maxime eos minima,
+          assumenda nemo dignissimos ipsa suscipit neque delectus! Iste earum
+          porro voluptate illo nostrum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam natus
+          voluptatem totam ad, nam, voluptates labore at expedita rerum rem
+          temporibus illum sit ut quisquam. Animi voluptas nisi veniam odio sit.
+          Enim aliquid odio eos ratione odit optio omnis ipsam fugit blanditiis
+          facere culpa, labore modi quas, nobis maiores voluptatibus libero
+          dolor, obcaecati magni iure quaerat quae autem? Rem voluptatibus
+          laborum quis eum error dolores! Est inventore voluptatibus
+          perspiciatis dolorum voluptas ab eum libero quas consequatur iure modi
+          iste accusamus veniam dolor architecto veritatis maxime eos minima,
+          assumenda nemo dignissimos ipsa suscipit neque delectus! Iste earum
+          porro voluptate illo nostrum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam natus
+          voluptatem totam ad, nam, voluptates labore at expedita rerum rem
+          temporibus illum sit ut quisquam. Animi voluptas nisi veniam odio sit.
+          Enim aliquid odio eos ratione odit optio omnis ipsam fugit blanditiis
+          facere culpa, labore modi quas, nobis maiores voluptatibus libero
+          dolor, obcaecati magni iure quaerat quae autem? Rem voluptatibus
+          laborum quis eum error dolores! Est inventore voluptatibus
+          perspiciatis dolorum voluptas ab eum libero quas consequatur iure modi
+          iste accusamus veniam dolor architecto veritatis maxime eos minima,
+          assumenda nemo dignissimos ipsa suscipit neque delectus! Iste earum
+          porro voluptate illo nostrum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam natus
+          voluptatem totam ad, nam, voluptates labore at expedita rerum rem
+          temporibus illum sit ut quisquam. Animi voluptas nisi veniam odio sit.
+          Enim aliquid odio eos ratione odit optio omnis ipsam fugit blanditiis
+          facere culpa, labore modi quas, nobis maiores voluptatibus libero
+          dolor, obcaecati magni iure quaerat quae autem? Rem voluptatibus
+          laborum quis eum error dolores! Est inventore voluptatibus
+          perspiciatis dolorum voluptas ab eum libero quas consequatur iure modi
+          iste accusamus veniam dolor architecto veritatis maxime eos minima,
+          assumenda nemo dignissimos ipsa suscipit neque delectus! Iste earum
+          porro voluptate illo nostrum!
+        </p>
+      </Link>
+    </div>
+  );
 }
